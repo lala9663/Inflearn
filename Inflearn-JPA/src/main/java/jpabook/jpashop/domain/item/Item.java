30eb85c092421d54a14f2fe085e18e916831d393
lala9663 <lala96632040@gmail.com>
2023-05-30 14:12:14 +0900

    entity 오류 수정 & 완성

--- Inflearn-JPA/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ Inflearn-JPA/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,23 +1,24 @@
-package jpabook.jpashop.domain;
+package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Getter
-@Setter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
 public abstract class Item {
-
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
     @Column(name = "item_id")
     private Long id;
     private String name;
     private int price;
     private int stockQuantity;
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<Category>();
 }
--- Inflearn-JPA/src/main/java/jpabook/jpashop/domain/item/Movie.java
+++ Inflearn-JPA/src/main/java/jpabook/jpashop/domain/item/Movie.java
@@ -1,2 +1,15 @@
-package jpabook.jpashop.domain;public class Movie {
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("M")
+@Getter @Setter
+public class Movie extends Item {
+    private String director;
+    private String actor;
 }
