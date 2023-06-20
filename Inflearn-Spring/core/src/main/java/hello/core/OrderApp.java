40fa322d1ca7673df5136e659b04212693232191
lala9663 <lala96632040@gmail.com>
2023-06-20 15:31:34 +0900

    파일 오류로 파일 이동 완료

---
+++ Inflearn-Spring/core/src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,23 @@
+package hello.core;
+
+import hello.core.member.*;
+import hello.core.order.*;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Long memberId1 = 2L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order1 = orderService.createOrder(memberId1, "itemB", 8000);
+
+        System.out.println("order = " + order);
+        System.out.println("order = " + order1);
+
+    }
+}
