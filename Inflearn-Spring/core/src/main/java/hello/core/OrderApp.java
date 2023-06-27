0c056998a4842ec24de0b20b0db0020ad6ff950c
lala9663 <lala96632040@gmail.com>
2023-06-27 20:54:57 +0900

    @bean 등록 하는 방법 설정

--- Inflearn-Spring/core/src/main/java/hello/core/OrderApp.java
+++ Inflearn-Spring/core/src/main/java/hello/core/OrderApp.java
@@ -2,15 +2,22 @@ package hello.core;
 
 import hello.core.member.*;
 import hello.core.order.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
 //        MemberService memberService = new MemberServiceImpl();
 //        OrderService orderService = new OrderServiceImpl();
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
