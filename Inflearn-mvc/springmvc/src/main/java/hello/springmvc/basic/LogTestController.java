07d7a5e3b2c0c8a3d1e7d128bd17d79891ae86fb
lala9663 <lala96632040@gmail.com>
2023-07-30 15:53:44 +0900

    log 레벨에 따른 출력 결과

---
+++ Inflearn-mvc/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,25 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController     // restController 는 반환 값으로 뷰를 찾는 것이 아니라, HTTP 메세지 바디에 바로 입력한다. 따라서 ok 메세지를 받을 수있다.
+public class LogTestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+        System.out.println("name = " + name);   // 가능하면 이거 쓰지말고 log 사용하기
+
+        log.trace(" trace log ={}", name);  //
+        log.debug(" debug log ={}", name);  // 개발서버에서 보기 debug
+        log.info(" info log ={}", name);    // 운영시스템에서 봐야할 정보
+        log.warn(" warn log ={}", name);    // 경고
+        log.error(" error log ={}", name);  // 에러(빨리 확인)
+
+        return "ok";
+    }
+}
