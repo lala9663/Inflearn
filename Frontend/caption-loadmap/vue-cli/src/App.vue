99c9edf45c483343709aed18fa64480116492796
lala9663 <lala96632040@gmail.com>
2023-12-06 20:30:45 +0900

    싱글 파일 컴포넌트

--- Frontend/caption-loadmap/vue-cli/src/App.vue
+++ Frontend/caption-loadmap/vue-cli/src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header
+      v-bind:propsdata ="str"
+      v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
+
+// new Vue({
+//   data: {
+//     str: 'hi'
+//   }
+// })
 
 export default {
-  name: 'App',
+  data: function() {
+    return {
+      str: 'hi'
+    }
+  },
   components: {
-    HelloWorld
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function(){
+      this.str = 'hi';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
