cb974b2fd49610105b7a841f3bd0c56dade92783
lala9663 <lala96632040@gmail.com>
2024-02-14 13:29:50 +0900

    인프런 스터디
    기초 문법 var,let의 scope

---
+++ Frontend/JohnAhn-로드맵/기초 문법/console/script.js
@@ -0,0 +1,20 @@
+console.log('Hello world');
+console.log(1234567);
+console.log(false);
+var greeting = '안녕하세요';
+console.log(greeting);
+console.log({ a: "a", b: "b" });
+console.table({ a: "a", b: "b" });
+
+console.error('Error!');
+console.warn('Warning');
+
+console.time('console time');
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.timeEnd('console time');
+
+console.clear();
---
+++ Frontend/JohnAhn-로드맵/기초 문법/var,let,const/script.js
@@ -0,0 +1,85 @@
+// var greeting = 'hello';
+// console.log(greeting); // hello
+
+// var greeting = 'hi';
+// console.log(greeting); //hi
+
+// greeting = 'how are you?';
+// console.log(greeting); //how are you?
+
+
+// let greeting = 'hello';
+// console.log(greeting); // hello
+
+// let greeting = 'hi';
+// console.log(greeting);
+// //Uncaught SyntaxError: Identifier 'greeting' has already been declared
+
+// greeting = 'how are you?';
+// console.log(greeting); //how are you?
+
+// const greeting = 'hello';
+// console.log(greeting); // hello
+
+// const greeting = 'hi';
+// console.log(greeting);
+// //Uncaught SyntaxError: Identifier 'greeting' has already been declared
+
+// greeting = 'how are you?';
+// console.log(greeting);
+// //Uncaught TypeError: Assignment to constant variable
+
+// const arrayList = [1, 2, 3];
+// arrayList.push(4);
+// console.log(arrayList);
+
+// const objectList = { a: 'a', b: 'b' };
+// objectList.c = "c"
+// console.log(objectList);
+
+// function func() {
+//     if (true) {
+//         var a = 'a';
+//         console.log(a); // 'a'
+//     }
+//     console.log(a); // 'a'
+// }
+
+// func();
+// console.log(a); // ReferenceError: a is not defined
+
+
+
+// function func() {
+//     if (true) {
+//         let a = 'a';
+//         console.log(a); // 'a'
+//     }
+//     console.log(a); // ReferenceError: a is not defined
+// }
+
+// console.log(a); // ReferenceError: a is not defined
+
+let hello;
+console.log(hello);
+
+
+
+// a = 5;
+// console.log(a);
+// var a; // 5
+
+
+// console.log(greeting);
+// // Uncaught ReferenceError: Cannot access 'greeting' before initialization
+// const greeting = "hello";
+
+// console.log(greeting);
+// // undefined
+// let greeting = "hello";
+
+// func(); //hoisting test
+
+// function func() {
+//     console.log("hoisting test");
+// }
