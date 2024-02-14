cfe18568ac3f385a60030bfdd705bf314974b28d
lala9663 <lala96632040@gmail.com>
2024-02-14 16:26:26 +0900

    js-기초 연산

---
+++ Frontend/JohnAhn-로드맵/기초 문법/js-calculation/script.js
@@ -0,0 +1,27 @@
+const num1 = 20;
+const num2 = 10;
+
+let val;
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math Object
+val = Math.E;
+val = Math.PI;
+val = Math.round(val);
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.abs(-2);
+val = Math.min(2,3,4,5,6,7,-1);
+val = Math.max(2,3,4,5,6,7,-1);
+val = Math.random();
+
+// 1 ~ 20;
+val = Math.floor(Math.random() * 20 + 1);
+
+
+console.log(val);
