8808ac8e921651d18a8053ed7e78ccd2e0be6f92
lala9663 <lala96632040@gmail.com>
2025-10-11 22:04:53 +0900

    map 메소드 활용

--- Frontend/JohnAhn-로드맵/따라하며 배우는 리액트 A-Z/REACT-TODO-APP/src/App.jsx
+++ Frontend/JohnAhn-로드맵/따라하며 배우는 리액트 A-Z/REACT-TODO-APP/src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Component } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default class App extends Component{
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  btnStyle = {
+    color: '#fff',
+    border: 'none',
+    padding: '5px 10px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+  }
+
+  getStyle = () => {
+    return {
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: 'none' // 할 일 완료시 줄 긋기 효과 제거
+    }
+  }
+
+  todoData = [
+    {
+      id: 1,
+    title: '공부하기',
+    completed: true,
+    },
+    {
+      id: 2,
+      title: '청소하기',
+      completed: false
+    }
+    ]
+
+  render() {
+    return (
+      <div className='container'>
+        <div className='todoBlock'>
+          <div className='title'>
+            <h1>할 일 목록</h1>
+          </div>
+
+          {
+            this.todoData.map((data) => (
+              <div key={data.id} style={this.getStyle()}>
+                <input type='checkbox' defaultChecked={false} />
+                {data.title }
+                <button style={this.btnStyle}>X</button>
+              </div>
+            ))
+          }
+          
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
+  }  
 }
-
-export default App
